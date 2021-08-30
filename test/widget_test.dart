bfe6d2942fd6d10b3375e7100b060678e647585b
SeungAhSon <48306744+SeungAhSon@users.noreply.github.com>
2021-08-31 00:56:35 +0900

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:gugujeoljeol/main.dart';
+import 'package:mirinae_gugu/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

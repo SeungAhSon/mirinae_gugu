ed82136a96f0a7ef5341a69c7dd77eaaa9b7f87d
jaewan0114 <42615916+jaewan0114@users.noreply.github.com>
2021-09-16 01:15:40 +0900

    파이어베이스 초기화 코드 추가

--- lib/screens/login/main.dart
+++ lib/screens/login/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mirinae_gugu/screens/login/first_screen.dart';
 import 'package:mirinae_gugu/screens/login/login_page.dart';
 import 'package:mirinae_gugu/screens/login/sign_in.dart';
 
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatefulWidget {
   @override

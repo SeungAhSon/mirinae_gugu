f70444148414549dd450eed06f75eda2c32d3f4f
jaewan0114 <42615916+jaewan0114@users.noreply.github.com>
2021-09-23 03:53:08 +0900

    리스트뷰 작업중

---
+++ lib/video/src/components/Video_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class VideoWidget extends StatelessWidget{
+  const VideoWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      color: Colors.green,
+    );
+  }
+}
--- lib/video/src/pages/home.dart
+++ lib/video/src/pages/home.dart
@@ -1,77 +1,89 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mirinae_gugu/video/src/app.dart';
+import 'package:mirinae_gugu/video/src/components/Video_appbar.dart';
+import 'package:mirinae_gugu/video/src/components/Video_widget.dart';
+
 
 class Home extends StatelessWidget{
-  static const PrimaryColor1 = const Color(0xFF5DB6F8);
+
   const Home({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context){
-
     return SafeArea(
       child: Scaffold(
-        body: WillPopScope(    // <- Scaffold body만 감싼다.
-          child: Center(
-          ),
-          onWillPop: () {
 
-            return Future(() => false);
-          },
-        ),
         appBar: AppBar(
 
           backgroundColor: Colors.white,
           title: Text('구구절절',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 20,
-          ),),
-          centerTitle: true,
-         leading: IconButton(
-                  onPressed: () {
-                  Navigator.pop(context);
-                },
-                color: Colors.black,
-                iconSize: 22,
-                icon: Icon(Icons.arrow_back),
-
-         ),
-
-          actions: <Widget>[IconButton(
-            icon: Icon(Icons.bookmark_outline),
-              iconSize: 22,
+            style: TextStyle(
               color: Colors.black,
-              onPressed: () {
-              print('shopping cart button is clicked');
-              }
+              fontSize: 20,
+            ),),
+          centerTitle: true,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            color: Colors.black,
+            iconSize: 25,
+            icon: Icon(Icons.arrow_back),
+
+          ),
+
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.bookmark_outline),
+                iconSize: 25,
+                color: Colors.black,
+                onPressed: () {
+                  print('GD');
+                }
             ),
           ],
-         ),
         ),
-      );
-      // CustomScrollView(slivers: [
-      //       (
-      //         backgroundColor: Colors.white,
-      //         expandedHeight: 80,
-      //         title: Container(
-      //           child: Text("AppBar"),
-      //
-      //         ),
-      //       ),
-      //       SliverList(delegate: SliverChildBuilderDelegate(
-      //           (context, index){
-      //             return Padding(
-      //                 padding:const EdgeInsets.all(8.0),
-      //                 child: Container(height: 300,color: Colors.grey,),
-      //             );
-      //
-      //           },
-      //         childCount: 10,
-      //       ),
-      //       ),
-      //     ],
+          body: ListView.builder(
+            padding: const EdgeInsets.all(1),
+            itemBuilder: (BuildContext context, int index) {
+              return GestureDetector(
+                onTap: (){
+                },
+                child: VideoWidget(),
+              );
+            },
+              ),
+
+          ),
 
 
-
+    );
   }
 }
+
+    //     return SafeArea(
+//       child: CustomScrollView
+//       (slivers: [
+//         SliverAppBar(
+//             title: CustomAppBar(),
+//             floating: true,
+//             snap: true
+//         ),
+//         SliverList(
+//           delegate: SliverChildBuilderDelegate(
+//                 (context, index){
+//                   return Padding(
+//                       padding:const EdgeInsets.all(8.0),
+//                       child: Container(height: 150,color: Colors.grey,),
+//                   );
+//
+//                 },
+//               childCount: 10,
+//             ),
+//             ),
+//           ],
+//
+//     ),
+// );
+//   }
+// }

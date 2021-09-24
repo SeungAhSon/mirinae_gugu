bc6574dbe5ef6239086fb7c383e87ffcd67092a7
jaewan0114 <42615916+jaewan0114@users.noreply.github.com>
2021-09-24 20:06:19 +0900

    ㄹㅇㄹㅇ반응형 완성.

--- lib/video/src/pages/home.dart
+++ lib/video/src/pages/home.dart
@@ -10,8 +10,12 @@ import 'package:mirinae_gugu/video/src/components/Video_widget.dart';
 class Home extends StatelessWidget{
   const Home({Key? key}) : super(key: key);
 
+
+
+
   @override
   Widget build(BuildContext context){
+    var height2 = AppBar().preferredSize.height;
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -46,8 +50,9 @@ class Home extends StatelessWidget{
             ),
           ],
         ),
-
+        
           body: Column(
+            
             children: [
 
               //상단 슬라이드
@@ -55,17 +60,17 @@ class Home extends StatelessWidget{
                 padding: EdgeInsets.only(right: MediaQuery.of(context).size.width/(30/29),), //상단 슬라이드
                 child: Container(
 
-                  height: MediaQuery.of(context).size.height/(150/1),
+                  height: (MediaQuery.of(context).size.height - height2 - MediaQuery.of(context).padding.top) * 0.01,
                   color: Colors.blue.withOpacity(0.6),
                 ),
               ),
 
               //상단 슬라이드 밑에 선
               Padding(
-                padding: EdgeInsets.only(bottom:MediaQuery.of(context).size.height/(500/1)), //상단 슬라이드 밑에 선
+                padding: EdgeInsets.only(bottom:(MediaQuery.of(context).size.height - height2 - MediaQuery.of(context).padding.top) * 0.003,), //상단 슬라이드 밑에 선
                 child: Container(
-                  height: 1,
-                  color: Colors.grey.withOpacity(0.2),
+                  height: (MediaQuery.of(context).size.height - height2 - MediaQuery.of(context).padding.top) * 0.001,
+                  color: Colors.grey.withOpacity(0.3),
                 ),
               ),
 
@@ -74,7 +79,7 @@ class Home extends StatelessWidget{
                 padding: EdgeInsets.only(bottom: 0),//left:MediaQuery.of(context).size.width/(12/1),right: MediaQuery.of(context).size.width/(12/1),),
                 child: Container(
 
-                height: MediaQuery.of(context).size.height/(3/1),
+                height: (MediaQuery.of(context).size.height - height2 - MediaQuery.of(context).padding.top) * 0.36,
                 color: Colors.grey.withOpacity(0.5),
               ),
             ),
@@ -82,7 +87,7 @@ class Home extends StatelessWidget{
 
               //중간 여백
               SizedBox(
-                height: MediaQuery.of(context).size.height/(80/1),
+                height: (MediaQuery.of(context).size.height - height2 - MediaQuery.of(context).padding.top) * 0.013,
               ),
 
 
@@ -91,7 +96,7 @@ class Home extends StatelessWidget{
                 padding: EdgeInsets.only(bottom: 0),//left:MediaQuery.of(context).size.width/(12/1),right: MediaQuery.of(context).size.width/(12/1),),
                 child: Container(
 
-                height: MediaQuery.of(context).size.height/(3/1),
+                height: (MediaQuery.of(context).size.height - height2 - MediaQuery.of(context).padding.top) * 0.36,
                 color: Colors.grey.withOpacity(0.5),
               ),
               ),
@@ -99,9 +104,9 @@ class Home extends StatelessWidget{
 
               //텍스트
               Padding(
-                padding: EdgeInsets.only(top: MediaQuery.of(context).size.height/(80/1), bottom: 0),//left:MediaQuery.of(context).size.width/(12/1),right: MediaQuery.of(context).size.width/(12/1),),
+                padding: EdgeInsets.only(top: (MediaQuery.of(context).size.height - height2 - MediaQuery.of(context).padding.top) * 0.013, bottom: 0),//left:MediaQuery.of(context).size.width/(12/1),right: MediaQuery.of(context).size.width/(12/1),),
                 child: Container(
-                  height: MediaQuery.of(context).size.height/(13/1),
+                  height: (MediaQuery.of(context).size.height - height2 - MediaQuery.of(context).padding.top) * 0.1,
                   color: Colors.grey.withOpacity(0.5),
                 ),
               ),
@@ -110,9 +115,9 @@ class Home extends StatelessWidget{
 
               //하단 바 상단선
               Padding(
-                padding: EdgeInsets.only(top: MediaQuery.of(context).size.height/(80/1),bottom:0), //상단 슬라이드 밑에 선
+                padding: EdgeInsets.only(top: (MediaQuery.of(context).size.height - height2 - MediaQuery.of(context).padding.top) * 0.013,bottom:0), //상단 슬라이드 밑에 선
                 child: Container(
-                  height: 1,
+                  height: (MediaQuery.of(context).size.height - height2 - MediaQuery.of(context).padding.top) * 0.002,
                   color: Colors.grey.withOpacity(0.5),
                 ),
               ),
@@ -122,7 +127,7 @@ class Home extends StatelessWidget{
               Padding(
                 padding: EdgeInsets.only(left: MediaQuery.of(context).size.width/(70/1), right: MediaQuery.of(context).size.width/(70/1)),//left:MediaQuery.of(context).size.width/(12/1),right: MediaQuery.of(context).size.width/(12/1),),
                 child: Container(
-                  height: MediaQuery.of(context).size.height/(13/1),
+                  height: (MediaQuery.of(context).size.height - height2 - MediaQuery.of(context).padding.top) * 0.08,
                   color: Colors.white.withOpacity(0),
                     child: _buttonZone()
                 ),
@@ -130,15 +135,15 @@ class Home extends StatelessWidget{
 
               //하단 바 아래
               Padding(
-                padding: EdgeInsets.only(bottom:MediaQuery.of(context).size.height/(300/1)), //상단 슬라이드 밑에 선
+                padding: EdgeInsets.only(bottom:0), //0.063남음
                 child: Container(
-                  height: 1,
+                  height: (MediaQuery.of(context).size.height - height2 - MediaQuery.of(context).padding.top) * 0.002,
                   color: Colors.grey.withOpacity(0.5),
 
                 ),
               ),
 
-
+//0.043남음
 
 
             ],

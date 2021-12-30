9a2d454d3d0cafab3c92fbc92f5822c96365356d
SeungAh Son <gongsoonyee@gmail.com>
2021-12-30 19:55:09 +0900

    syllable button 사이즈 비율로

--- lib/video/src/widget/button_Syllable.dart
+++ lib/video/src/widget/button_Syllable.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mirinae_gugu/video/src/pages/1_Loading.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ButtonSyllable extends StatelessWidget {
   ButtonSyllable({
@@ -13,8 +14,8 @@ class ButtonSyllable extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: 50,
-      height: 50,
+      width: 50.0.w,
+      height: 50.0.h,
       child: RaisedButton(
         color: Colors.white,
         shape: RoundedRectangleBorder(
